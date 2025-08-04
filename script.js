e547bf40df4944f5dc6a35f8cc91d8f9a24c7ed9
Prakash Kumar <kumarprakash3909@gmail.com>
2025-08-05 03:31:45 +0530

    final

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// code for carousel
+//carousel
+
 var sliderMain = document.getElementById("carousel");
 var item = sliderMain.getElementsByClassName("card");
 
@@ -10,15 +11,15 @@ function prev(){
 }
 
 
-// code for video
+// video play/pause 
+
 const video = document.getElementById("myVideo");
 const playPauseBtn = document.getElementById("playPauseBtn");
 const playIcon = document.getElementById("playIcon");
 const pauseIcon = document.getElementById("pauseIcon");
 
-// Toggle video play/pause on button click
 playPauseBtn.addEventListener("click", (e) => {
-  e.stopPropagation(); // Prevent click from reaching video
+  e.stopPropagation(); 
   if (video.paused) {
     video.play();
   } else {
@@ -28,8 +29,6 @@ playPauseBtn.addEventListener("click", (e) => {
     pauseIcon.style.display = "none";
   }
 });
-
-// Pause video on click while it's playing
 video.addEventListener("click", () => {
   if (!video.paused) {
     video.pause();
@@ -38,13 +37,9 @@ video.addEventListener("click", () => {
     pauseIcon.style.display = "none";
   }
 });
-
-// Hide button when video starts playing
 video.addEventListener("play", () => {
   playPauseBtn.style.display = "none";
 });
-
-// Show button again when video ends
 video.addEventListener("ended", () => {
   playPauseBtn.style.display = "block";
   playIcon.style.display = "block";
@@ -52,35 +47,19 @@ video.addEventListener("ended", () => {
 });
 
 
+// request a dish modal
+
+function openModal() {
+  document.getElementById("requestDishModal").style.display = "flex";
+  document.body.style.overflow = "hidden";
+  document.body.classList.add("blurred");
+}
+
+function closeModal() {
+  document.getElementById("requestDishModal").style.display = "none";
+  document.body.style.overflow = "auto";
+  document.body.classList.remove("blurred");
+}
 
 
 
-
-
-
-
-
-
-// modal2 = document.getElementsByClassName("background-blured")[0];
-
-// console.log(modal2)
-
-// function openModal2(){
-//     modal2.classList.remove("hide2");
-//     // const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
-//     const body = document.body;
-//     body.style.height = '100vh';
-//     body.style.overflowY = 'hidden';
-//     window.scrollTo(0, parseInt(scrollY || '0') * -1);
-// }
-
-// function closeModal2(){
-//     modal2.classList.add("hide2");
-//     const body = document.body;
-//     const scrollY = body.style.top;
-//     body.style.position = '';
-//     body.style.top = '';
-//     body.style.height = '';
-//     body.style.overflowY = '';
-    
-// }
