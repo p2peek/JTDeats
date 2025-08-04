8ba04eb443a3ee85fa87bac829e3d24717a3ac62
Prakash Kumar <kumarprakash3909@gmail.com>
2025-08-05 01:07:13 +0530

    req a dish

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// code for carousel
 var sliderMain = document.getElementById("carousel");
 var item = sliderMain.getElementsByClassName("card");
 
@@ -9,7 +10,7 @@ function prev(){
 }
 
 
-
+// code for video
 const video = document.getElementById("myVideo");
 const playPauseBtn = document.getElementById("playPauseBtn");
 const playIcon = document.getElementById("playIcon");
@@ -49,3 +50,37 @@ video.addEventListener("ended", () => {
   playIcon.style.display = "block";
   pauseIcon.style.display = "none";
 });
+
+
+
+
+
+
+
+
+
+
+
+// modal2 = document.getElementsByClassName("background-blured")[0];
+
+// console.log(modal2)
+
+// function openModal2(){
+//     modal2.classList.remove("hide2");
+//     // const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+//     const body = document.body;
+//     body.style.height = '100vh';
+//     body.style.overflowY = 'hidden';
+//     window.scrollTo(0, parseInt(scrollY || '0') * -1);
+// }
+
+// function closeModal2(){
+//     modal2.classList.add("hide2");
+//     const body = document.body;
+//     const scrollY = body.style.top;
+//     body.style.position = '';
+//     body.style.top = '';
+//     body.style.height = '';
+//     body.style.overflowY = '';
+    
+// }
